69551ff8137917c10896ec5f601532db9fd5d558
Rawah G <rawah.h@outlook.com>
2021-02-26 16:57:56 +0100

    updated

--- ex-2/exercise.js
+++ ex-2/exercise.js
@@ -1,3 +1,15 @@
-// write your JS here
-// remember to always test in the browser
-// to see if everything works correctly
+
+function tellFortune (job, place, partner, kids){
+    var fortune =  "You will be a  " + job + " in " + place + ", and married to " + partner + " with " + kids + " kids";
+  window.alert(fortune) ;
+};
+
+var n = window.prompt("give me a number of children");
+var z  = window.prompt("give me a partner's name" );
+var y  = window.prompt("give me an geographic location");
+var x = window.prompt("give me an job title");
+
+tellFortune (x, y, z, n)
+
+tellFortune("cfo", "roma", "linda", "0")
+tellFortune("ceo", "milano", "nora", "1")
