a02a6811e4d04019ade321defba7ff071e5b01a4
Rawah G <rawah.h@outlook.com>
2021-02-26 17:30:04 +0100

    updated

--- ex-3/exercise.js
+++ ex-3/exercise.js
@@ -1,3 +1,19 @@
-// write your JS here
-// remember to always test in the browser
-// to see if everything works correctly
+
+
+var colors = ["blue","grey","black"];
+
+console.log("My #1 choice is " + colors[0])
+
+console.log("My #2 choice is " + colors[1])
+
+console.log("My #3 choice is " + colors[2])
+
+var colors = {
+    first: "blue",
+    second: "grey",
+    third: "black",
+  };  
+
+  window.alert("my favorite color is "+ colors.first +", then "+ colors.second+", then " + colors.third);
+
+  window.alert("my favorite color is blue, then grey, then black");
