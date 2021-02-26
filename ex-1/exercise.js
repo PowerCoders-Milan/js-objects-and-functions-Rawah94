6380225b32969bd0c54f9e5e792b2435faa9d2a2
Rawah G <rawah.h@outlook.com>
2021-02-26 14:46:01 +0100

    updated

--- ex-1/exercise.js
+++ ex-1/exercise.js
@@ -1,3 +1,13 @@
-// write your JS here
-// remember to always test in the browser
-// to see if everything works correctly
+function phraseBuilder(noun, verb, adverb, adjective){
+    var phrase =  noun  + " " +  verb + " " +  adverb  + " " + adjective;
+    return phrase ;
+};
+
+var noun = window.prompt("give me a noun");
+var verb  = window.prompt("give me a verb" );
+var adverb  = window.prompt("give me an adverb");
+var adjective = window.prompt("give me an adjective");
+
+window.alert(phraseBuilder(noun, verb, adverb, adjective));
+
+
